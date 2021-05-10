b784c713543b5cc5e7fc49c5c3bd71c3b124c307
Olena Sevastian <olena.sevastian@altexsoft.com>
2021-05-10 15:23:11 +0300

    add methond in service and mock db

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,5 +1,45 @@
-const getAll = async () =>
-  // TODO: mock implementation. should be replaced during task development
-  [];
+const users = {
+  '56bcf12e-fccc-4c04-bc42-50f1b656654f': {
+    id: '56bcf12e-fccc-4c04-bc42-50f1b656654f',
+    name: 'Elena',
+    login: 'ellankz',
+    password: 'idkWhatDoYouThink55',
+  },
+  '8774283f-7e31-4e98-bed9-927d599fbb6c': {
+    id: '8774283f-7e31-4e98-bed9-927d599fbb6c',
+    name: 'Jonny',
+    login: 'jonnyCash456',
+    password: 'qwerty',
+  },
+};
 
-export default { getAll };
+const getAll = async () => Object.values(users);
+
+const getOne = async (id) => users[id];
+
+const createOne = async (userData) => {
+  if (!users[userData.id]) {
+    users[userData.id] = userData;
+    return userData;
+  }
+  return null;
+};
+
+const updateOne = async (userData) => {
+  if (users[userData.id]) {
+    const updatedUser = { ...users[userData.id], ...userData };
+    users[userData.id] = updatedUser;
+    return updatedUser;
+  }
+  return null;
+};
+
+const deleteOne = async (id) => {
+  if (users[id]) {
+    delete users[id];
+    return true;
+  }
+  return null;
+};
+
+export default { getAll, getOne, createOne, updateOne, deleteOne };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,10 @@ import usersRepo from './user.memory.repository.js';
 
 export const getAll = () => usersRepo.getAll();
 
-export const getSingle = () => usersRepo.getSingle();
+export const getOne = () => usersRepo.getOne();
+
+export const createOne = () => usersRepo.createOne();
+
+export const updateOne = () => usersRepo.updateOne();
+
+export const deleteOne = () => usersRepo.deleteOne();
