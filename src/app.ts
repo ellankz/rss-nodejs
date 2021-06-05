203f0d4c34b8d0d4acc3b28ff88d9bb8ba78bcea
Olena Sevastian <olena.sevastian@altexsoft.com>
2021-06-05 18:18:51 +0300

    add logging and base error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
 import path from 'path';
@@ -6,11 +6,15 @@ import path from 'path';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import { logError, logRequest } from './logging/winston.logger';
+import { ErrorHandler, handleError } from './errors/error';
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 app.use(express.json());
+
+app.use(logRequest);
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
@@ -25,5 +29,13 @@ app.use('/', (req, res, next) => {
 app.use('/users', userRouter);
 app.use('/boards', taskRouter);
 app.use('/boards', boardRouter);
+
+app.use((err: Error | ErrorHandler, _req: Request, res: Response, next: NextFunction) => {
+  const error = err instanceof ErrorHandler ? err : new ErrorHandler(500, 'Internal server error');
+  logError(error);
+
+  handleError(error, res);
+  next(err);
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
 import { PORT } from './common/config';
 import app from './app';
+import { logError } from './logging/winston.logger';
+import { ErrorHandler } from './errors/error';
 
-app.listen(PORT, () => process.stdout.write(`App is running on http://localhost:${PORT}`));
+process
+  .on('unhandledRejection', reason => {
+    const message = `Unhandled Rejection at Promise: ${reason instanceof Error ? reason.message : 'unknow error'}` 
+    logError(new ErrorHandler(500, `Unhandled Rejection at Promise: ${message}`));
+    process.exit(1);
+  })
+  .on('uncaughtException', err => {
+    logError(new ErrorHandler(500, `Uncaught Exception: ${err.message}`));
+    process.exit(1);
+  });
+
+app.listen(PORT, () => process.stdout.write(`App is running on http://localhost:${PORT}\n`));
