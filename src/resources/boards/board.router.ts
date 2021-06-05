b031f2fea9fa38ab54f57f4879b6cfae77958ab0
Olena Sevastian <olena.sevastian@altexsoft.com>
2021-06-05 18:19:55 +0300

    handle errors in board router

--- src/resources/boards/board.router.ts
+++ src/resources/boards/board.router.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
+import { ErrorHandler } from '../../errors/error';
 import {
   createOne,
   deleteOne,
@@ -11,42 +13,74 @@ const router = express.Router();
 
 router
   .route('/')
-  .get(async (_req, res) => {
-    const boards = await getAll();
-    if (boards) {
-      res.json(boards);
-    } else {
-      res.status(404);
-      res.json('Not found.');
+  .get(async (_req, res, next) => {
+    try {
+      const boards = await getAll();
+      if (boards) {
+        res.json(boards);
+      } else {
+        throw new ErrorHandler(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND);
+      }
+    } catch (error) {
+      next(error);
     }
   })
-  .post(async (req, res) => {
-    const board = await createOne(req.body);
-    res.status(201);
-    res.json(board);
+  .post(async (req, res, next) => {
+    try {
+      const { columns, title } = req.body;
+      const columnsValid = !columns || (Array.isArray(columns) && columns.every((column: {[key: string]: string}) => !!column['title']));
+      if (!title || !columnsValid){
+        throw new ErrorHandler(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST);
+      }
+      const board = await createOne(req.body);
+      res.status(201);
+      res.json(board);
+    } catch (error) {
+      next(error);
+    }
   });
 
 router
   .route('/:boardId')
-  .get(async (req, res) => {
-    const board = await getOne(req.params.boardId);
-    if (board) {
-      res.json(board);
-    } else {
-      res.status(404);
-      res.json('Not found.');
+  .get(async (req, res, next) => {
+    try {
+      const board = await getOne(req.params.boardId);
+      if (board) {
+        res.json(board);
+      } else {
+        throw new ErrorHandler(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND)
+      }
+    } catch (error) {
+      next(error);
     }
   })
-  .put(async (req, res) => {
-    const board = await updateOne(req.params.boardId, req.body);
-    res.json(board);
+  .put(async (req, res, next) => {
+    try {
+      const {title, columns} = req.body;
+      const boardValid = title || (columns && Array.isArray(columns));
+      const columnsValid = !columns || (Array.isArray(columns) && columns.every((column: {[key: string]: string}) => !!column['title']));
+      if (!boardValid || !columnsValid) {
+        throw new ErrorHandler(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST);
+      }
+      const board = await updateOne(req.params.boardId, req.body);
+      if (!board) {
+        throw new ErrorHandler(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND);
+      }
+      res.json(board);
+    } catch (error) {
+      next(error);
+    }
   })
-  .delete(async (req, res) => {
-    const isDeleted = await deleteOne(req.params.boardId);
-    if (isDeleted) {
-      res.status(204).json({ Error: 'Board has been deleted' });
-    } else {
-      res.status(404).json({ Error: 'Board not found' });
+  .delete(async (req, res, next) => {
+    try {
+      const isDeleted = await deleteOne(req.params.boardId);
+      if (isDeleted) {
+        res.status(204).json({ Error: 'Board has been deleted' });
+      } else {
+        throw new ErrorHandler(StatusCodes.NOT_FOUND, ReasonPhrases.NOT_FOUND);
+      }
+    } catch (error) {
+      next(error);
     }
   });
 
